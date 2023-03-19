d468777f43694e9cbe861ed97170e195d68afb81
PhaserArray <me@phaserarray.me>
2023-03-19 06:21:17 +0200

    Update load.py
    Update to EDMC 5.8.1

--- Solo Indicator/load.py
+++ Solo Indicator/load.py
@@ -1,17 +1,29 @@
-import sys
+import logging
 import tkinter as tk
 from monitor import monitor
+from config import appname
 
-this = sys.modules[__name__]
-
-def plugin_start3(plugin_dir):
+def plugin_start3(plugin_dir = ""):
     return "Solo Indicator"
 
+class This():
+    def __init__(self):
+        self.logger = logging.getLogger(f'{appname}.{plugin_start3()}')
+        self.logger.info("Starting Solo Indicator")
+
+this = This()
+
 def plugin_app(parent):
-    for thing in parent.winfo_children():
-        if thing._name == "cmdr":
-            this.cmdrlabel = thing
+    this.parent = parent
 
 def journal_entry(cmdr, is_beta, system, station, entry, state):
+    if not this.parent:
+        return
+    
+    cmdr = this.parent.nametowidget(f".{appname.lower()}.cmdr")
+    if not cmdr:
+        this.logger.error("Could not find cmdr widget")
+        return
+
     if monitor.mode.lower() == "solo":
-        this.cmdrlabel["text"] = '%s / %s' % (monitor.cmdr, 'Solo')
+        cmdr["text"] = '%s / %s' % (monitor.cmdr, 'Solo')
